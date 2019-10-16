6b85ca766ce0ef79f5933b8e71fe7de2b5f52dff
Robert <20180840@ce.pucmm.edu.do>
2019-10-15 20:36:40 -0400

    punto 4

--- punto4/punto4Lib.c
+++ punto4/punto4Lib.c
@@ -14,6 +14,18 @@ struct producto _busqueda_binaria_recursiva(int id, struct producto prods[], int
  * @return
  */
 struct producto busqueda_binaria(int id, struct producto productos[], int n){
+
+    struct producto prod;
+    if(n == 0){
+        prod.id = -1;
+        return prod;
+    }else if( n == 1){
+
+        return
+
+    }
+
+
     return _busqueda_binaria_recursiva(id, productos, 0, n-1);
 }
 
@@ -27,5 +39,17 @@ struct producto busqueda_binaria(int id, struct producto productos[], int n){
  * @return
  */
 struct producto _busqueda_binaria_recursiva(int id, struct producto prods[], int ii, int is){
+    if(is >= ii){
+        int pivot = ii + (is - 1) /2;
 
+        if(prods[pivot].id == id)
+            return prods[pivot];
+        if(prods[pivot].id > id)
+            return _busqueda_binaria_recursiva(id, prods, ii, pivot-1);
+
+        return _busqueda_binaria_recursiva(id, prods, pivot+1, is);
+    }
+
+
+    return -1;
 }
